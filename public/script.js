c3d139ff8ebad3aa20bee19ea8dc47d0092406b1
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-03-29 22:25:33 +0100

    First commit

--- public/script.js
+++ public/script.js
@@ -34,7 +34,7 @@ function createBoard() {
     const shuffledSymbols = shuffle([...cardSymbols, ...cardSymbols]);
     totalPairsDisplay.textContent = totalPairs; // Muestra el total de pares
     gameBoard.innerHTML = ''; // Limpia el tablero anterior
-    movesDisplay.innerHTML = `Movimientos: ${moves} / ${max_moves}`;   
+    movesDisplay.innerHTML = `${moves} / ${max_moves}`;   
 
     shuffledSymbols.forEach(symbol => {
         const cardElement = document.createElement('div');
