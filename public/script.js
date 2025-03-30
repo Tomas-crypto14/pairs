6055e8553697cbd28b85a19b2d2e224e4efc513d
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-03-30 18:44:56 +0200

    First commit

--- public/script.js
+++ public/script.js
@@ -129,7 +129,8 @@ function checkWinCondition() {
     if (matchedPairs === totalPairs) {
         winMessage.style.display = 'block';
         playAgainButton.style.display = 'inline-block';
-    }else if(moves === max_moves){
+    }
+    if(moves === max_moves){
         endGame();
     }
 }
